737a65873cfdcda51cfc936c0082bab5c9269ad1
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-10-30 13:11:29 +0530

    refactor: Refactor renderState function

--- routes/dashboard/portals/feedbacks-24x7/index.js
+++ routes/dashboard/portals/feedbacks-24x7/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-	res.send("Feedbacks 24x7");
+	res.renderState('dashboard/portals/feedbacks-24x7');
 });
 
 module.exports = router;
