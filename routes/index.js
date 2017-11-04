318fc7e9918174eeca257ba05e0f1b53197884f7
Nischay Ram Mamidi <NischayPro@gmail.com>
2017-11-04 19:30:50 +0530

    code: add teams layout

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
  	res.render('index');
 });
+router.get('/team', function(req, res, next) {
+	res.render('team');
+});
 
 module.exports = router;
