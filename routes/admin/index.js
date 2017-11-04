eb1a8308ae3ec6595fe4b721659b9b7d02b3fb19
Nischay Ram Mamidi <NischayPro@gmail.com>
2017-11-04 20:30:14 +0530

    code: promises and auto create Super Admin when not existing

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,31 +3,49 @@ var router = express.Router();
 
 var adminsModel = require('../../schemas/admins');
 var portalsModel = require('../../schemas/portals');
+var settingsModel = require('../../schemas/settings');
+
+
 
 /* Configure middleware for portal permissions */
 
-let securityCheck = function(req, res, next){
+let securityCheck = function (req, res, next) {
 
     var reqPortal = (req.originalUrl.split('/'))[2];
 
-    portalsModel.find({ name: reqPortal, active: true, admin: true }, function(err, result){
-        if(err) {
-            res.render('custom_errors', {message: "Server error", details: "An unexpected error occoured. Contact Instruction Division software team for assistance."});
+    portalsModel.find({
+        name: reqPortal,
+        active: true,
+        admin: true
+    }, function (err, result) {
+        if (err) {
+            res.render('custom_errors', {
+                message: "Server error",
+                details: "An unexpected error occoured. Contact Instruction Division software team for assistance."
+            });
         }
-        if(result.length > 0 || req.user.superUser) {
-            if(req.user.portals.indexOf(reqPortal) >= 0 || req.user.superUser){
+        if (result.length > 0 || req.user.superUser) {
+            if (req.user.portals.indexOf(reqPortal) >= 0 || req.user.superUser) {
                 next();
             } else {
-                res.render('custom_errors', {message: 'You do not have permission to access this portal', details: 'Contact Instruction Division software team for assistance.'});
+                res.render('custom_errors', {
+                    message: 'You do not have permission to access this portal',
+                    details: 'Contact Instruction Division software team for assistance.'
+                });
             }
         } else {
-            res.render('custom_errors', {message: 'This portal has been disabled by the Administrator', details: 'Contact Instruction Division software team for assistance.'});
+            res.render('custom_errors', {
+                message: 'This portal has been disabled by the Administrator',
+                details: 'Contact Instruction Division software team for assistance.'
+            });
         }
     });
 };
 
-portalsModel.find({ admin: true }, function(err, portals){
-    portals.forEach(function(portal) {
+portalsModel.find({
+    admin: true
+}, function (err, portals) {
+    portals.forEach(function (portal) {
         var portalPath = require('./portals/' + portal.name);
         router.use('/' + portal.name, securityCheck, portalPath);
     });
@@ -42,57 +60,101 @@ var googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var session = require('express-session');
 var keys = require('../../config');
 
-adminPassport.serializeUser(function(user, done) {
+adminPassport.serializeUser(function (user, done) {
     return done(null, user.emails[0].value);
 });
 
-adminPassport.deserializeUser(function(id, done) {
-	adminsModel.find({email: id}, function(err, result){
-        if(err) {
+adminPassport.deserializeUser(function (id, done) {
+    adminsModel.find({
+        email: id
+    }, function (err, result) {
+        if (err) {
             console.log(err);
         }
-		return done(err, result[0]);
-	});
+        return done(err, result[0]);
+    });
 });
 
 
 adminPassport.use(new googleStrategy({
-	clientID:     keys.googleClientID,
+    clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: keys.googleAdminCallback,
-    passReqToCallback   : true
-	}, function(request, accessToken, refreshToken, profile, done){
-		return done(null, profile);
+    passReqToCallback: true
+}, function (request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
 }));
 
-router.use(session({ secret: 'ADMIN-BPHC-ID' }));
+router.use(session({
+    secret: 'ADMIN-BPHC-ID'
+}));
 router.use(adminPassport.initialize());
 router.use(adminPassport.session());
 
-router.get('/login', adminPassport.authenticate('google', { scope: ['profile', 'email']}));
+router.get('/login', adminPassport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
-router.get('/auth/google/callback', 
-  adminPassport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-
-    adminsModel.find({email: req.user.emails[0].value}, function(err, result){
-    	if(err){
-    		res.render('custom_errors', {message: "Server error", details: "An unexpected error occoured. Contact Instruction Division software team for assistance."});
-    	}
-    	if(result.length == 0){
-    		req.session.destroy(function(){
-    			res.render('custom_errors', {message: "You are not an administrator", details: "This google account is not registered as an administrator."});
-    		});
-    	} else {
-    		res.redirect('/admin' + result[0].home);
-    	}
+router.get('/auth/google/callback',
+    adminPassport.authenticate('google', {
+        failureRedirect: '/login'
+    }),
+    function (req, res) {
+        settingsModel.find({
+            name: "config"
+        }, function (err, result) {
+            if (err) {
+                res.render('custom_errors', {
+                    message: "Server Connection Error",
+                    details: "Server Connection Error has occured"
+                });
+            }
+            if (result.length == 0) {
+                var CreateAdmin = adminsModel.create({
+                    email: req.user.emails[0].value,
+                    name: req.user.displayName,
+                    superUser: true
+                });
+                var SettingsAdd = settingsModel.create({
+                    name: "config",
+                    description: "Sets the Super Admin during first run.",
+                    value: [req.user.emails[0].value]
+                });
+                var CheckAdmin = (function () {
+                    req.session.destroy(function () {
+                        res.redirect('/admin/login');
+                    });
+                });
+                var AfterCreateAdmin = CreateAdmin.then(SettingsAdd).then(CheckAdmin);
+            } else {
+                adminsModel.find({
+                    email: req.user.emails[0].value
+                }, function (err, result) {
+                    if (err) {
+                        res.render('custom_errors', {
+                            message: "Server error",
+                            details: "An unexpected error occoured. Contact Instruction Division software team for assistance."
+                        });
+                    }
+                    if (result.length == 0) {
+                        req.session.destroy(function () {
+                            res.render('custom_errors', {
+                                message: "You are not an administrator",
+                                details: "This google account is not registered as an administrator."
+                            });
+                        });
+                    } else {
+                        res.redirect('/admin');
+                    }
+                });
+            }
+        });
     });
- });
 
-router.get('/logout', function(req, res){
-	req.session.destroy(function(err){
-		res.redirect('/');
-	});
+router.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        res.redirect('/');
+    });
 });
 
 /********* adminPassport Config End *********/
@@ -100,18 +162,20 @@ router.get('/logout', function(req, res){
 
 /*Add end points for non logged in users above this line*/
 
-router.use(function(req, res, next){
-	if(!(req.user)){
-		res.redirect('/admin/login');
-	} else {
+router.use(function (req, res, next) {
+    if (!(req.user)) {
+        res.redirect('/admin/login');
+    } else {
         next();
-    }	
+    }
 });
 
 /* Below end points are availible only to logged in users */
 
-router.get('/', function(req, res, next) {
- 	res.render('custom_errors', {message: "Welcome, " + req.user.name});
+router.get('/', function (req, res, next) {
+    res.render('custom_errors', {
+        message: "Welcome, " + req.user.name
+    });
 });
 
 module.exports = router;
