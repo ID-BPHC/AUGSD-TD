831a5a6afc5364d31b3418663daabfda30a364a2
Nischay Ram Mamidi <NischayPro@gmail.com>
2017-11-10 06:19:05 +0530

    code: added stepper material select post logic

--- routes/dashboard/portals/feedbacks-24x7/index.js
+++ routes/dashboard/portals/feedbacks-24x7/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
 	res.renderState('dashboard/portals/feedbacks-24x7');
 });
 
+router.get('/step-1', function (req, res, next) {
+	res.renderState('dashboard/portals/feedbacks-24x7/step1');
+});
+
+router.post('/step-2', function (req, res, next) {
+	console.log(req.body);
+	res.renderState('dashboard/portals/feedbacks-24x7/step2', {params: req.params});
+});
 module.exports = router;
