a5ee6600394daacf03736598d9bd3d058f193a64
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-18 21:58:02 +0530

    rom-booking: Add check for finding correct room based on capacity and room-map

--- routes/dashboard/portals/room-booking-student/index.js
+++ routes/dashboard/portals/room-booking-student/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var fq = require('fuzzquire');
-var roomsSchema = fq('schemas/rooms');
+var roomsModel = fq('schemas/rooms');
+var moment = require('moment');
+
+var weekDayHash = {
+    "Mon": 0,
+    "Tue": 1,
+    "Wed": 2,
+    "Thu": 3,
+    "Fri": 4,
+    "Sat": 5,
+    "Sun": 6
+};
 
 router.get('/', function(req, res, next) {
     res.renderState('dashboard/portals/room-booking-student');
@@ -19,6 +30,58 @@ router.get('/step-3', function(req, res, next) {
     res.renderState('dashboard/portals/room-booking-student');
 });
 
+router.post('/step-2', function(req, res, next) {
+
+    var weekDay = parseInt(weekDayHash[(req.sanitize(req.body.date)).substring(0, 3)]);
+    var startHour = parseInt(req.sanitize(req.body['time-start']).substring(0, 2)) - 8;
+    var endHour = parseInt(req.sanitize(req.body['time-end']).substring(0, 2)) - 8;
+
+    let roomRegularSearch = roomsModel.find({
+        lectureCapacity: {
+            $gt: parseInt(req.sanitize(req.body.capacity))
+        }
+    }).lean().exec(function(err, rooms) {
+
+        if (err) {
+            res.renderState('custom_errors', {
+                message: "An unexpected error occoured",
+                details: "Contact Instruction Division software team for assistance"
+            });
+        }
+
+        if (rooms.length == 0) {
+            res.renderState('custom_errors', {
+                message: "No room with given capacity",
+                details: "Try booking multiple rooms with smaller capacity"
+            });
+        }
+
+        rooms.forEach(function(room, index) {
+
+            room.availible = true;
+
+            if (startHour >= 0 && startHour <= 9) {
+                for (i = startHour; i <= endHour; i++) {
+
+                    if (room.fixedClasses[weekDay][i] !== '') {
+                        room.availible = false;
+                        break;
+                    }
+
+                }
+            }
+
+        });
+        return rooms;
+    });
+
+    roomRegularSearch.then(function(rooms) {
+        rooms.forEach(function(element, index) {
+            console.log(element.availible + " " + element.number);
+        });
+    });
+
+});
 
 
 module.exports = router;
