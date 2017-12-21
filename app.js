17a98a008a56ac1fdc8f9e35d2f91abafb35846e
Nischay Mamidi <15060256+Nischay-Pro@users.noreply.github.com>
2017-12-21 14:17:56 +0530

    api: added support for api calls providing 3 endpoints (#17)

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var expressSanitizer = require('express-sanitizer');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var expressValidator = require('express-validator');
-var config = require('./config')
+var config = require('./config');
 var session = require('express-session');
 
 mongoose.connect(config.mongooseConnection);
 
+var api = require('./routes/api');
 var admin = require('./routes/admin');
 var dashboard = require('./routes/dashboard');
 var index = require('./routes');
@@ -47,18 +48,19 @@ app.use(auth.adminPassport.session());
 app.use(auth.userPassport.session());
 
 app.use('/admin', admin);
+app.use('/api', api);
 app.use('/dashboard', dashboard);
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -67,8 +69,8 @@ app.use(function(err, req, res, next) {
     res.locals.title = "Oh noes";
     res.locals.subtitle = "Our guys have been notified about this.";
     if (err.status == 404) {
-        res.locals.missing == true;
-        res.locals.title = "404 Couldn't find it"
+        res.locals.missing = true;
+        res.locals.title = "404 Couldn't find it";
         res.locals.subtitle = "You sure you typed the link correctly?";
     }
     // render the error page
