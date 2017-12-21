d71174a3d73b7d320934975b44cd4890317fb732
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-21 20:46:17 +0530

    mailer: Add HTML formatting to mails

--- utils/mailer.js
+++ utils/mailer.js
@@ -14,13 +14,22 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+var header = '<img src="cid:head@idbitshyd.com" width="100%"><br><br><br><font size="4">';
+var footer = '<br><br><br><font color="#000000">This is an auto-generated mail. Please do not reply.</font><br><font color="#000000"><strong><font color="#f1c232">+++++++++++++</font><font color="#6fa8dc">++++++++++++</font><font color="#ff0000">+++++<wbr>+++++++</font></strong></font><br><font color="#0000ff"><strong>A Vasan</strong><br></font><font color="#0000ff">Associate Dean, Instruction Division<br>BITS Pilani - Hyderabad Campus</font></font></font><br>';
+
 module.exports = {
 	send: function(params){
 		var mailOptions = {
 			from: config.mailUser,
 			to: params.email,
 			subject: params.subject,
-			text: params.body
+			html: header + params.body + footer,
+
+			attachments: [{
+				filename: 'head.png',
+				path: './assets/head.png',
+				cid: 'head@idbitshyd.com'
+			}]
 		};
 
 		transporter.sendMail(mailOptions, function(err, info) {
