de016457bfc646a0488c473ef632d76b92cbcfc7
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-25 13:53:31 +0530

    gulp: Minify

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use(function(req, res, next) {
         res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
         res.header('Expires', '-1');
         res.header('Pragma', 'no-cache');
-    }
+    };
     next();
 
 });
