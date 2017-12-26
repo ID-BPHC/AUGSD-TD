041c6ffaf4080997327ecb2df38f989ab31f0c18
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-26 15:16:53 +0530

    ta: Division work frontend done

--- routes/dashboard/portals/ta-application/index.js
+++ routes/dashboard/portals/ta-application/index.js
@@ -2,24 +2,35 @@ var express = require('express');
 var router = express.Router();
 var fq = require('fuzzquire');
 var coursesModel = fq('schemas/courses');
+var divisionsModel = fq('schemas/divisions');
 
-router.get('/', function(req, res, next) {
-    res.renderState('dashboard/portals/ta-application');
+router.get('/', function (req, res, next) {
+	res.renderState('dashboard/portals/ta-application');
 });
 
-router.get('/division', function(req, res, next) {
-    res.renderState('dashboard/portals/ta-application/apply-division');
+router.get('/division', function (req, res, next) {
+	divisionsModel.find({}).sort({ name: 1 }).exec(function (err, divisions) {
+		if (err) {
+			res.terminate(err);
+		}
+		res.renderState('dashboard/portals/ta-application/apply-division', {
+			divisions: divisions
+		});
+	});
 });
 
-router.get('/course', function(req, res, next) {
+router.get('/course', function (req, res, next) {
 
-    coursesModel.find({}).sort({
-        courseID: 1
-    }).exec(function(err, courses) {
-        res.renderState('dashboard/portals/ta-application/apply-course', {
-            courses: courses
-        });
-    });
+	coursesModel.find({}).sort({
+		courseID: 1
+	}).exec(function (err, courses) {
+		if (err) {
+			res.terminate(err);
+		}
+		res.renderState('dashboard/portals/ta-application/apply-course', {
+			courses: courses
+		});
+	});
 });
 
 module.exports = router;
