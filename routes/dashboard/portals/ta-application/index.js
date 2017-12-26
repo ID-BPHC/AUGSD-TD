0d185ef6f7b8d03eb4079db63fb5f4def91d78ea
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-26 13:52:13 +0530

    ta: Course work application frontend done

--- routes/dashboard/portals/ta-application/index.js
+++ routes/dashboard/portals/ta-application/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fq = require('fuzzquire');
+var coursesModel = fq('schemas/courses');
 
 router.get('/', function(req, res, next) {
     res.renderState('dashboard/portals/ta-application');
 });
 
 router.get('/division', function(req, res, next) {
-    res.renderState('dashboard/portals/ta-application/apply-division', {
-        type: "division"
-    });
+    res.renderState('dashboard/portals/ta-application/apply-division');
 });
 
 router.get('/course', function(req, res, next) {
-    res.renderState('dashboard/portals/ta-application/apply-course', {
-        type: "course"
+
+    coursesModel.find({}).sort({
+        courseID: 1
+    }).exec(function(err, courses) {
+        res.renderState('dashboard/portals/ta-application/apply-course', {
+            courses: courses
+        });
     });
 });
 
