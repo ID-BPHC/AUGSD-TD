336953bfc81dc3696bb1fceb83fafea0cb7a3300
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-28 14:28:23 +0530

    room-booking: Add server side validation of form data

--- routes/admin/portals/room-booking-faculty/index.js
+++ routes/admin/portals/room-booking-faculty/index.js
@@ -4,26 +4,28 @@ var fq = require('fuzzquire');
 var roomBookingFaculty = fq('middleware/room-booking');
 var moment = require('moment');
 
+const { check, validationResult } = require('express-validator/check');
+
 // GET Requests
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.renderState('room-booking');
 });
 
-router.get('/step-1', function(req, res, next) {
+router.get('/step-1', function (req, res, next) {
     res.renderState('room-booking/step1');
 });
 
-router.get('/step-2', function(req, res, next) {
+router.get('/step-2', function (req, res, next) {
     res.renderState('room-booking');
 });
 
-router.get('/step-3', function(req, res, next) {
+router.get('/step-3', function (req, res, next) {
     res.renderState('room-booking');
 });
 
-router.get('/view', function(req, res, next) {
-    roomBookingFaculty.view(req.sanitize(req.user.email), function(err, bookings) {
+router.get('/view', function (req, res, next) {
+    roomBookingFaculty.view(req.sanitize(req.user.email), function (err, bookings) {
         if (err) {
             return res.terminate("Error");
         }
@@ -33,8 +35,8 @@ router.get('/view', function(req, res, next) {
     });
 });
 
-router.get('/cancel/:id', function(req, res, next) {
-    roomBookingFaculty.cancel(req.sanitize(req.params.id), req.sanitize(req.user.email), function(err) {
+router.get('/cancel/:id', function (req, res, next) {
+    roomBookingFaculty.cancel(req.sanitize(req.params.id), req.sanitize(req.user.email), function (err) {
         if (err) {
             return res.terminate("Error");
         }
@@ -45,13 +47,35 @@ router.get('/cancel/:id', function(req, res, next) {
 
 // POST Requests
 
-router.post('/step-2', function(req, res, next) {
+router.post('/step-2', [
+    check('capacity').exists().withMessage('No Capacity').isNumeric().withMessage('Invalid Capacity').not().isEmpty().withMessage('No Capacity'),
+    check('purpose').exists().withMessage('No Purpose').not().isEmpty().withMessage('No Purpose'),
+    check('date').exists().withMessage('No Date').not().isEmpty().withMessage('No Date Specified'),
+    check('phone').exists().withMessage('No Phone').isNumeric().withMessage('Invalid Phone').isLength({ min: 10, max: 10 }).withMessage('Invalid Phone').not().isEmpty().withMessage('No Phone'),
+    check('av').exists().withMessage('No AV Value').isIn(['Yes', 'No']).withMessage('Invalid AV Value'),
+    check('le').exists().isIn(['Yes', 'No']).withMessage('Lecture/Exam not specified')
+], function (req, res, next) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.renderState('form-errors', { errors: errors.mapped() });
+    }
 
     var startTime = new moment(req.sanitize(req.body.date) + " " + req.sanitize(req.body['time-start']), 'ddd DD MMM YYYY HH:mm').toDate();
     var endTime = new moment(req.sanitize(req.body.date) + " " + req.sanitize(req.body['time-end']), 'ddd DD MMM YYYY HH:mm').toDate();
+
+    if (endTime < startTime) {
+        return res.renderState('custom_errors', {
+            redirect: "/admin/room-booking-faculty/step-1",
+            timeout: 5,
+            supertitle: "Time Error",
+            message: "End Time was chosen before Start Time",
+        });
+    }
+
     var exam = (req.sanitize(req.body.le) == "Yes") ? true : false;
 
-    roomBookingFaculty.getRooms(req.sanitize(req.body.date), req.sanitize(req.body['time-start']), req.sanitize(req.body['time-end']), req.sanitize(req.body.capacity), exam, true, function(err, rooms) {
+    roomBookingFaculty.getRooms(req.sanitize(req.body.date), req.sanitize(req.body['time-start']), req.sanitize(req.body['time-end']), req.sanitize(req.body.capacity), exam, true, function (err, rooms) {
 
         if (err) {
             return res.terminate("Error");
@@ -81,11 +105,11 @@ router.post('/step-2', function(req, res, next) {
     });
 });
 
-router.post('/step-3', function(req, res, next) {
+router.post('/step-3', function (req, res, next) {
 
     var room = req.sanitize(req.body.room);
 
-    roomBookingFaculty.makeBooking(room, req.session.rooms, req.session.endTime, req.session.startTime, req.user.email, req.session.av, req.session.purpose, req.session.phone, true, function(err, result) {
+    roomBookingFaculty.makeBooking(room, req.session.rooms, req.session.endTime, req.session.startTime, req.user.email, req.session.av, req.session.purpose, req.session.phone, true, function (err, result) {
         if (err) {
             return res.terminate("Error");
         }
