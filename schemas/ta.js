a46c306d4e211931c43b82d9f7a5cfe6043436ba
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-28 09:18:17 +0530

    schemas: Add default values to TA schema

--- schemas/ta.js
+++ schemas/ta.js
@@ -4,21 +4,54 @@ var Schema = mongoose.Schema;
 var taSchema = new Schema({
     student: String,
     type: String, // C or D,
-    cgpa: Number,   
-    hours: Number, 
-    idApproval: String, // P or A or R
-    icApproval: String, // P or A or R
-    icRemarks: String, // instructor remarks
-    hdStudents: Number,
+    cgpa: {
+        type: Number,
+        default: 0
+    },   
+    hours: {
+        type: Number,
+        default: 0
+    }, 
+    idApproval: {
+        type: String,
+        default: "P"
+    }, // P or A or R
+    icApproval: {
+        type: String,
+        default: "P"
+    }, // P or A or R
+    icRemarks: {
+        type: String,
+        default: "None"
+    }, // instructor remarks
+    hdStudents: {
+        type: Number,
+        default: 0
+    },
 
     // For course
-    course: String,
-    registeredStudents: Number,
-    numInstructors: Number,
-    hodApproval: String, // P or A or R,,
+    course: {
+        type: String,
+        default: "None"
+    },
+    registeredStudents: {
+        type: Number,
+        default: 0
+    },
+    numInstructors: {
+        type: Number,
+        default: 0
+    },
+    hodApproval: {
+        type: String,
+        default: "P"
+    }, // P or A or R,,
 
     // For Division
-    division: String
+    division: {
+        type: String,
+        default: "Nil"
+    }
 });
 
 var model = mongoose.model('ta', taSchema);
