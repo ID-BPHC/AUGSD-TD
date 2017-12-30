12ae9f9f7b72aba43480f13e1c6be661912f4807
Nischay Ram Mamidi <NischayPro@gmail.com>
2017-12-30 17:45:14 +0530

    ui: added team UI

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fq = require('fuzzquire');
+var users = fq('users');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index');
 });
-router.get('/team', function(req, res, next) {
-    res.render('team');
+router.get('/team', function (req, res, next) {
+    res.render('team', {
+        users: users,
+        column: 3,
+        totalModules: 17
+    });
 });
-router.get('/type', function(req, res, next) {
+router.get('/type', function (req, res, next) {
     res.render('type');
 });
 
---
+++ routes/users.js
@@ -0,0 +1,107 @@
+let users = {
+    "Vasan Arunachalam": {
+        "name": "Vasan Arunachalam",
+        "title": "Associate Dean",
+        "avatar": "/images/team/1.jpg",
+        "social": {}
+    },
+    "Prof. PK Sahoo": {
+        "name": "Prof. PK Sahoo",
+        "title": "Timetable (Incharge)",
+        "avatar": "/images/team/2.jpg",
+        "social": {}
+    },
+    "Dr. Ramakrishnan Ganeshan": {
+        "name": "Dr. Ramakrishnan Ganeshan",
+        "title": "Timetable",
+        "avatar": "/images/team/3.jpg",
+        "social": {}
+    },
+    "Dr. Supradeepan K": {
+        "name": "Dr. Supradeepan K",
+        "title": "Timetable",
+        "avatar": "/images/team/4.jpg",
+        "social": {}
+    },
+    "Prof. R Punna Rao": {
+        "name": "Prof. R Punna Rao",
+        "title": "Test Scheduling and Invigilation Allotment",
+        "avatar": "/images/team/5.jpg",
+        "social": {}
+    },
+    "Prof. B Mishra": {
+        "name": "Prof. B Mishra",
+        "title": "Feedback and Monitoring (Incharge)",
+        "avatar": "/images/team/6.jpg",
+        "social": {}
+    },
+    "Prof. D. Sriram": {
+        "name": "Prof. D. Sriram",
+        "title": "Feedback and Monitoring",
+        "avatar": "/images/team/7.jpg",
+        "social": {}
+    },
+    "Prof. Sridhar Raj​u": {
+        "name": "Prof. Sridhar Raj​u",
+        "title": "Feedback and Monitoring",
+        "avatar": "/images/team/8.jpg",
+        "social": {}
+    },
+    "Dr. Balaji Gopalan": {
+        "name": "Dr. Balaji Gopalan",
+        "title": "Project Allotment & First Degree Teaching Assistantship",
+        "avatar": "/images/team/9.jpg",
+        "social": {}
+    },
+    "Prof. R Krishnan": {
+        "name": "Prof. R Krishnan",
+        "title": "Handouts, Textbooks, Scrutiny, Question Paper & Solutions, Mid ​Semester Grades, Class Room Booking",
+        "avatar": "/images/team/10.jpg",
+        "social": {}
+    },
+    "Dr. Aruna Malapati": {
+        "name": "Dr. Aruna Malapati",
+        "title": "CMS & Software Development",
+        "avatar": "/images/team/11.jpg",
+        "social": {}
+    },
+    "Mr. G Naresh": {
+        "name": "Mr.G Naresh",
+        "title": "Staff",
+        "avatar": "/images/team/12.jpg",
+        "social": {}
+    },
+    "Mr. Anil Zedia": {
+        "name": "Mr. Anil Zedia",
+        "title": "Staff",
+        "avatar": "/images/team/13.jpg",
+        "social": {}
+    },
+    "Mrs. N Srilakshmi": {
+        "name": "Mrs. N Srilakshmi",
+        "title": "Staff",
+        "avatar": "/images/team/14.jpg",
+        "social": {}
+    },
+    "Nischay Ram Mamidi": {
+        "name": "Nischay Ram Mamidi",
+        "title": "System Administrator & Software Developer",
+        "avatar": "/images/team/15.jpg",
+        "social": {}
+    },
+    "Sohail Rajdev": {
+        "name": "Sohail Rajdev",
+        "title": "Software Developer",
+        "avatar": "/images/team/16.jpg",
+        "social": {}
+    },
+    "Amit Mishra": {
+        "name": "Amit Mishra",
+        "title": "Software Developer",
+        "avatar": "/images/team/17.jpg",
+        "social": {}
+    }
+
+}
+
+module.exports = users;
