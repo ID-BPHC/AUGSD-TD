d8aaef5de35724b3250ff4abee492803a173025f
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-12-30 13:06:51 +0530

    ta: IC Approval Portal Initial Commit

---
+++ routes/admin/portals/ta-app-ic/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+	res.renderState('admin/portals/ta-app-ic');
+});
+
+module.exports = router;
--- schemas/courses.js
+++ schemas/courses.js
@@ -5,6 +5,7 @@ var coursesSchema = new Schema({
     courseID: String,
     name: String,
     ic: String, // Stores email id of the course's IC
+    hod: String, // Email ID of the hod
     sections: [{
         section: String,
         instructors: [String] // This will store emails of intstructors for a particular section
