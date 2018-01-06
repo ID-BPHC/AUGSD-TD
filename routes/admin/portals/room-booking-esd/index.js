65e2a0fcc9bd099f150086e91b0510d5ca7c3984
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-01-06 11:37:28 +0530

    room-booking: Add ESD portal

---
+++ routes/admin/portals/room-booking-esd/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var fq = require('fuzzquire');
+var bookingsModel = fq('schemas/room-bookings');
+var moment = require('moment');
+
+router.get('/', function (req, res, next) {
+	bookingsModel.find({ start: { $gt: new moment() } }, null, { sort: { start: 1 } }, function (err, bookings) {
+		if (err) {
+			return res.terminate(err);
+		}
+		return res.renderState('admin/portals/room-booking-esd', { bookings: bookings });
+	});
+});
+
+module.exports = router;
