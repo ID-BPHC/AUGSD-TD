074cb13a9eea11c277b10aa00f1392b267781841
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-03-19 21:24:45 +0530

    proj-allot-1: Add aggregation for heads and projects

--- routes/dashboard/portals/project-allotment-student/index.js
+++ routes/dashboard/portals/project-allotment-student/index.js
@@ -1,8 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next){
-	res.send("Project Allotment");
+var fq = require('fuzzquire');
+var Promise = require('promise');
+
+var projectsModel = fq('schemas/projects');
+var headsModel = fq('schemas/project-heads');
+
+router.get('/', function (req, res, next) {
+
+	projectsModel.aggregate(
+		[{
+			$lookup: {
+				from: "projectheads",
+				localField: "instructor",
+				foreignField: "instructor",
+				as: "instructorForeign"
+			}
+		}, {
+			$project: {
+				"department": "$instructorForeign.department",
+				"instructorName": "$instructorForeign.name",
+				title: 1,
+				description: 1,
+				instrutor: 1,
+				name: 1,
+				updated: 1,
+				type: 1
+			}
+		}, {
+			$unwind: "$department"
+		}, {
+			$unwind: "$instructorName"
+		}],
+		function (err, result) {
+			if (err) {
+				console.log(err);
+				return res.terminate("Aggregate Error");
+			}
+			return res.json(result);
+		});
+
 });
 
 module.exports = router;
