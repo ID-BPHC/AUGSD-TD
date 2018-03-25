76c0677a1c0b9c49464c2479145a27fda9bd06f5
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-03-25 20:22:58 +0530

    proj-2: Add route to view projects (admin area)

--- routes/admin/portals/project-applications/index.js
+++ routes/admin/portals/project-applications/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 var fq = require('fuzzquire');
 
-router.get('/', function(req, res, next){
-	res.send('Project Applications');
+var projectsModel = fq('schemas/projects');
+
+router.get('/', function (req, res, next) {
+	projectsModel.find({ instructor: req.sanitize(req.user.email) }, function (err, projects) {
+
+		if (err) {
+			console.log(err);
+			return res.terminate("Could not find projects");
+		}
+
+		return res.renderState('admin/portals/project-applications', {
+			projects: projects
+		});
+	});
+});
+
+router.get('/project/:id', function (req, res, next) {
+	res.send("applications");
 });
 
 module.exports = router;
