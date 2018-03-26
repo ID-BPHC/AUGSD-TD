8f53eccd8be0825930f275ed70a00bc81f5da94b
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-03-26 11:51:18 +0530

    proj-2: Add route to view applications

--- routes/admin/portals/project-applications/index.js
+++ routes/admin/portals/project-applications/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 var fq = require('fuzzquire');
 
 var projectsModel = fq('schemas/projects');
+var applicationsModel = fq('schemas/project-applications');
 
 router.get('/', function (req, res, next) {
 	projectsModel.find({ instructor: req.sanitize(req.user.email) }, function (err, projects) {
 
 		if (err) {
 			console.log(err);
-			return res.terminate("Could not find projects");
+			return res.terminate(err);
 		}
 
 		return res.renderState('admin/portals/project-applications', {
@@ -19,7 +20,43 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/project/:id', function (req, res, next) {
-	res.send("applications");
+	projectsModel.findOne({ _id: req.sanitize(req.params.id) }, function (err, project) {
+
+		if (err) {
+			console.log(err);
+			return res.terminate(err);
+		}
+
+		if (!project) {
+			res.renderState('custom_errors', {
+				redirect: "/admin/project-applications",
+				timeout: 2,
+				supertitle: ".",
+				callback: "/",
+				message: "Validation Error",
+				details: "Project Not Found"
+			});
+		}
+
+		if (project.instructor !== req.sanitize(req.user.email)) {
+			return res.status(403).end();
+		} else {
+			applicationsModel.find({ project: project._id }, function (err, applications) {
+
+				if (err) {
+					console.log(err);
+					return res.terminate(err);
+				}
+
+				return res.renderState('admin/portals/project-applications/view', {
+					project: project,
+					applications: applications
+				});
+
+			});
+		}
+
+	});
 });
 
 module.exports = router;
