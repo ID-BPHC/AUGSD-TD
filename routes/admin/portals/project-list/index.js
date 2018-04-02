2f67d0f83d5d5418a0230ae941e5104ba5a42e62
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-04-03 00:03:07 +0530

    project-list: Add route to get list of applicants

--- routes/admin/portals/project-list/index.js
+++ routes/admin/portals/project-list/index.js
@@ -5,8 +5,51 @@ var fq = require('fuzzquire');
 var projectsModel = fq('schemas/projects');
 var applicationsModel = fq('schemas/project-applications');
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
 	return res.renderState('admin/portals/project-list');
+});
+
+router.get('/export/:status', function (req, res, next) {
+
+	var status = req.sanitize(req.params.status);
+	console.log(status);
+	applicationsModel.aggregate([{
+		$match: {
+			status: status
+		}
+	}, {
+		$lookup: {
+			from: 'projects',
+			localField: 'project',
+			foreignField: '_id',
+			as: 'projectForeign'
+		}
+	}, {
+		$lookup: {
+			from: 'students',
+			localField: 'student',
+			foreignField: 'email',
+			as: 'studentForeign'
+		}
+	}, {
+		$lookup: {
+			from: 'projectheads',
+			localField: 'projectForeign.instructor',
+			foreignField: 'instructor',
+			as: 'instructorForeign'
+		}
+	}, {
+		$project: {
+			'Student Name': '$studentForeign.name',
+			'ID Number': '$studentForeign.idNumber',
+			'Instructor': '$instructorForeign.name',
+			'Instructor Email': '$instructorForeign.instructor',
+			'Project': '$projectForeign.title'
+		}
+	}], function (err, list) {
+		console.log(list);
+	});
+
 });
 
 module.exports = router;
