e55be54c775d2118e51de3352156cd02d2f0ec41
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-04-15 21:51:45 +0530

    proj-update: Add view to display project applications

--- routes/dashboard/portals/project-update/index.js
+++ routes/dashboard/portals/project-update/index.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req,res,next)=>{
-	res.renderState("dashboard/portals/project-update");
+var fq = require('fuzzquire');
+var applicationsModel = fq('schemas/project-applications');
+
+router.get('/', (req, res, next) => {
+	applicationsModel.aggregate([{
+			$match: {
+				student: req.sanitize(req.user.email)
+			}
+		},
+		{
+			$lookup: {
+				from: "projects",
+				localField: "project",
+				foreignField: "_id",
+				as: "projectForeign"
+			}
+		},
+		{
+			$project: {
+				status: 1,
+				updated: 1,
+				"title": "$projectForeign.title"
+			}
+		},
+		{
+			$unwind: "$title"
+		}
+	], function (err, applications) {
+		if (err) {
+			console.log(err);
+			return res.terminate("Could not find applications");
+		}
+		return res.renderState("dashboard/portals/project-update", {
+			applications: applications
+		});
+	});
 });
 
-router.get('/:id', (req,res,next)=>{
+router.get('/view/:id', (req, res, next) => {
 	res.renderState("dashboard/portals/project-update/update");
 });
 
