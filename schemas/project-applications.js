58bcb6108217d018a679242c59d41e0358cd4ae5
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-04-15 21:51:05 +0530

    schemas: Add elective type field to project-applications schema

--- schemas/project-applications.js
+++ schemas/project-applications.js
@@ -7,6 +7,7 @@ var projectApplicationsSchema = new Schema({
     cgpa: Number,
     experience: String,
     courseCode: String,
+    disciplinary: Boolean,
     status: {
         type: String,
         default: "P"
