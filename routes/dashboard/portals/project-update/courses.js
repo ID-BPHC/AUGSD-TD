de834b169e5a3e447a5fc63555c7850138bcd471
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-04-16 15:42:13 +0530

    proj-update: Add course data

---
+++ routes/dashboard/portals/project-update/courses.js
@@ -0,0 +1,66 @@
+let courses = {
+	"Mechanical Engineering": {
+		"sop": ["ME F266", "MF F266"],
+		"lop": ["ME F366", "ME F367", "MF F366", "MF F367"],
+		"dop": ["ME F376", "ME F377", "MF F376", "MF F377"]
+	},
+	"Computer Sci & Information Sys": {
+		"sop": ["CS F266"],
+		"lop": ["CS F366", "CS F367"],
+		"dop": ["CS F376", "CS F377"]
+	},
+	"Economics and Finance": {
+		"sop": ["ECON F266"],
+		"lop": ["ECON F366", "ECON F367"],
+		"dop": ["ECON F376", "ECON F377"]
+	},
+	"Humanities and Social Sciences": {
+		"sop": ["HSS F266"],
+		"lop": [],
+		"dop": []
+	},
+	"Pharmacy": {
+		"sop": ["PHA F266"],
+		"lop": ["PHA F366", "PHA F367"],
+		"dop": ["PHA F376", "PHA F377"]
+	},
+	"Mathematics": {
+		"sop": ["MATH F266"],
+		"lop": ["MATH F366", "MATH F367"],
+		"dop": ["MATH F376", "MATH F377"]
+	},
+	"Civil Engineering": {
+		"sop": ["CE F266"],
+		"lop": ["CE F366", "CE F367"],
+		"dop": ["CE F376", "CE F377"]
+	},
+	"Chemical Engineering": {
+		"sop": ["CHE F266"],
+		"lop": ["CHE F366", "CHE F367"],
+		"dop": ["CHE F376", "CHE F377"]
+	},
+	"Biological Sciences": {
+		"sop": ["BIO F266"],
+		"lop": ["BIO F366", "BIO F367"],
+		"dop": ["BIO F376", "BIO F377"]
+	},
+	"Electrical & Electronics Engg": {
+		"sop": ["ECE F266", "EEE F266", "INSTR F266"],
+		"lop": ["ECE F366", "ECE F367", "EEE F366", "EEE F367", "INSTR F366", "INSTR F367"],
+		"dop": ["ECE F376", "ECE F377", "EEE F376", "EEE F377", "INSTR F376", "INSTR F377"]
+	},
+	"Chemistry": {
+		"sop": ["CHEM F266"],
+		"lop": ["CHEM F366", "CHEM F367"],
+		"dop": ["CHEM F376", "CHEM F377"]
+	},
+	"Physics": {
+		"sop": ["PHY F266"],
+		"lop": ["PHY F366", "PHY F367"],
+		"dop": ["PHY F376", "PHY F377"]
+	}
+};
+
+module.exports = {
+	courses: courses
+};
