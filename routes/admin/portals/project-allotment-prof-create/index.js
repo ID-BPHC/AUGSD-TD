7dc9a6ac3192fab35c728e9e68c64f37de8b19cc
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-06-01 13:04:19 +0530

    Refactor: Project Allotment - fixes

--- routes/admin/portals/project-allotment-prof-create/index.js
+++ routes/admin/portals/project-allotment-prof-create/index.js
@@ -4,128 +4,66 @@ var fq = require("fuzzquire");
 var projectsModel = fq("schemas/projects");
 var adminModel = fq("schemas/admins");
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   try {
-    let usertype = getProfessorType(req.user.email);
-    usertype.then(val => {
-      console.log(val);
-      if (val.admin) {
-        projectsModel.find({}).exec((err, result) => {
-          if (err) {
-            return res.terminate(err);
-          }
-          let promises2 = [];
-          let getdepartmentpromise = [];
-          result.forEach((object, index, array) => {
-            array[index].description =
-              array[index].description.substring(
-                0,
-                Math.min(array[index].description.length, 66)
-              ) + " ...";
-            promises2.push(getInstructorName(array[index].instructor));
-            getdepartmentpromise.push(
-              getProfessorDepartment(array[index].instructor)
-            );
-          });
-          Promise.all(promises2)
-            .then(vals => {
-              vals.forEach((object, index, array) => {
-                result[index].name = array[index];
-              });
-              return res.renderState(
-                "admin/portals/project-allotment-prof-create",
-                {
-                  projects: result
-                }
-              );
-            })
-            .catch(err => {
-              console.log(err);
-              res.terminate(err);
-            });
-        });
-      } else if (val.head) {
-          adminModel.
-          find({
-              department: val.department
-          }).lean()
-          .exec((err, projects) => {
-              if (err)  return res.terminate(err);
-              let projectquery = [];
-              projects.forEach((o, i, a) => {
-                  projectquery.push(getProfessorProjects(a[i].email));
-              });
-              let finalresult = [];
-              Promise.all(projectquery).then(vals => {
-                  vals.forEach(val => {
-                      finalresult = finalresult.concat(val);
-                  });
+      let usertype = getUserType(req.user.email);
+      usertype.then(val => {
+          console.log(val);
+          if (val.admin) {
+              projectsModel.find({}).exec((err, result) => {
+                  if (err) {
+                      return res.terminate(err);
+                  }
                   let promises2 = [];
-                  finalresult.forEach((object, index, array) => {
-                    array[index].description =
-                    array[index].description.substring(
-                      0,
-                      Math.min(array[index].description.length, 66)
-                    ) + " ...";
+                  result.forEach((object, index, array) => {
+                      array[index].description = array[index].description.substring(0, Math.min(array[index].description.length, 66)) + " ...";
                       promises2.push(getInstructorName(array[index].instructor));
                   });
                   Promise.all(promises2).then(vals => {
                       vals.forEach((object, index, array) => {
-                          finalresult[index].name = array[index];
+                          result[index].name = array[index];
                       });
                       return res.renderState('admin/portals/project-allotment-prof-create', {
-                          projects: finalresult
+                          projects: result
                       });
+                  }).catch(err => {
+                      console.log(err);
+                      res.terminate(err);
                   });
               });
-                });
-                return res.renderState(
-                  "admin/portals/project-allotment-prof-create",
-                  {
-                    projects: finalresult
-                  }
-                );
-      } else {
-        projectsModel
-          .find({
-            instructor: req.user.email
-          })
-          .lean(true)
-          .exec((err, result) => {
-            console.log(result);
-            if (err) {
-              return res.terminate(err);
-            }
-            let promises = [];
-            console.log(result);
-            result.forEach((object, index, array) => {
-              array[index].description =
-                array[index].description.substring(
-                  0,
-                  Math.min(array[index].description.length, 66)
-                ) + " ...";
-              promises.push(getInstructorName(array[index].instructor));
-              console.log(array[index].instructor);
-            });
-            Promise.all(promises).then(vals => {
-              vals.forEach((object, index, array) => {
-                result[index].name = array[index];
-              });
-              console.log(result);
-              return res.renderState(
-                "admin/portals/project-allotment-prof-create",
-                {
-                  projects: result
-                }
-              );
-            });
-          });
-      }
-    });
+          } else {
+              projectsModel.find({
+                  instructor: req.user.email
+              }).lean(true).exec(
+                  (err, result) => {
+                      console.log(result);
+                      if (err) {
+                          return res.terminate(err);
+                      }
+                      let promises = [];
+                      console.log(result);
+                      result.forEach((object, index, array) => {
+                          array[index].description = array[index].description.substring(0, Math.min(array[index].description.length, 66)) + " ...";
+                          promises.push(getInstructorName(array[index].instructor));
+                          console.log(array[index].instructor);
+                      });
+                      Promise.all(promises).then(vals => {
+                          vals.forEach((object, index, array) => {
+                              result[index].name = array[index];
+                          });
+                          console.log(result);
+                          return res.renderState('admin/portals/project-allotment-prof-create', {
+                              projects: result
+                          });
+                      });
+                  });
+          }
+      });
   } catch (err) {
-    return res.terminate(err);
+      return res.terminate(err);
   }
 });
+
 
 function getInstructorName(profmail) {
   return new Promise((resolve, reject) => {
@@ -145,93 +83,33 @@ function getInstructorName(profmail) {
   });
 }
 
-function getProfessorProjects(profmail) {
+function getUserType(profmail) {
   return new Promise((resolve, reject) => {
-    projectsModel
-      .find({
-        instructor: profmail
-      })
-      .lean()
-      .exec((err, result) => {
-        if (err) reject(err);
-        if (result != null && result != undefined) {
-          resolve(result);
-        } else {
-          resolve("Missing Professor Data");
-        }
-      });
-  });
-}
-
-function getProfessorDepartment(profmail) {
-    return new Promise((resolve, reject) => {
-        adminModel.find(
-        {
-            email: profmail
-        }, 
-        (err, result) => {
-            if (err)  reject(err);
-            if (result != null && result != undefined) {
-                resolve(result.department);
-            } else  resolve('Missing Professor Data');
-        });
-    });
-}
-
-function getProfessorType(profmail) {
-  return new Promise((resolve, reject) => {
-    adminModel.findOne(
-      {
-        email: profmail
-      },
-      (err, result) => {
-        if (err) reject(err);
-        if (result != null && result != undefined) {
-          if (result.superUser == true) {
-            resolve({
-              admin: true
-            });
-          } else {
-            projectHeadModel.findOne(
-              {
-                instructor: profmail
-              },
-              (err, result2) => {
-                if (err) reject(err);
-                if (result2 != null && result2 != undefined) {
-                  if (result2.head == true) {
-                    resolve({
-                      head: true,
-                      department: result2.department
-                    });
-                } else {
-                    adminModel.findOne({
-                        email: profmail
-                    }, (err, result2) => {
-                        if (err)
-                            reject(err);
-                        if (result2 != null && result2 != undefined) {
-                            if (result2.head == true) {
-                                resolve({
-                                    head: true,
-                                    department: result2.department
-                                });
-                            } else {
-                                resolve({
-                                    head: false,
-                                    department: result2.department
-                                });
-                            }
-                        }
-                    });
-                  }
-                }
+      adminModel.findOne({
+          email: profmail
+      }, (err, result) => {
+          if (err)
+              reject(err);
+          if (result != null && result != undefined) {
+              if (result.superUser == true) {
+                  resolve({
+                      admin: true
+                  });
+              } else {
+                  adminModel.findOne({
+                      email: profmail
+                  }, (err, result2) => {
+                      if (err)
+                          reject(err);
+                      if (result2 != null && result2 != undefined) {
+                          resolve({
+                              department: result2.department
+                          });
+                      }
+                  });
               }
-            );
           }
-        }
-      }
-    );
+      });
   });
 }
 
@@ -255,53 +133,7 @@ router.get("/view/:id", function(req, res, next) {
   }
 });
 
-router.post("/create", function(req, res, next) {
-  try {
-    let data = {
-      title: req.sanitize(req.body.title),
-      description: req.sanitize(req.body.description),
-      instructor: req.user.email
-    };
-    if (data.description.length == 0) {
-      data.description = "N/A";
-    }
-    if (data.title.length == 0) {
-      data.description = "N/A";
-    }
-    const typeproj = req.sanitize(req.body.lablist);
-    if (typeproj == "Lab Oriented Project (LOP)") {
-      data.type = "lop";
-    } else if (typeproj == "Design Oriented Project (DOP)") {
-      data.type = "dop";
-    } else {
-      data.type = "sop";
-    }
-    if (req.sanitize(req.body.proflist))
-      data.instructor = req
-        .sanitize(req.body.proflist)
-        .split("|")[1]
-        .replace(" ", "");
-    projectHeadModel.findOne(
-      {
-        instructor: data.instructor
-      },
-      (err, result) => {
-        if (err) return res.terminate(err);
-        if (result != undefined && result != null) {
-          // data.department = result.department;
-          projectsModel.create(data, (err, result) => {
-            if (err) {
-              return res.terminate(err);
-            }
-            return res.redirect("/admin/project-allotment-prof-create");
-          });
-        } else return res.terminate("Could not find professor record.");
-      }
-    );
-  } catch (err) {
-    return res.terminate(err);
-  }
-});
+
 
 router.get("/create", function(req, res, next) {
   try {
--- schemas/admins.js
+++ schemas/admins.js
@@ -2,13 +2,6 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var adminsSchema = new Schema({
-<<<<<<< eb08b1933ecb4e4422cec86f96235fae149bbaed
-  name: String,
-  email: String,
-  portals: [String],
-  home: String,
-  superUser: Boolean
-=======
     name: String,
     email: String,
     portals: [String],
@@ -20,14 +13,7 @@ var adminsSchema = new Schema({
     head: {
 		type: Boolean
 	},
->>>>>>> refactor: Merged projectHead schema with admins
 });
 
 var model = mongoose.model("admins", adminsSchema);
-
 module.exports = model;
-<<<<<<< eb08b1933ecb4e4422cec86f96235fae149bbaed
-=======
-
-
->>>>>>> refactor: Merged projectHead schema with admins
