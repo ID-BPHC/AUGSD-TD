79ad3467d3d54623dcbdfc74a19a13db612189e8
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-06-09 14:20:18 +0530

    feedbacks-refactor: Resolve merge conflicts and prettify files

--- routes/admin/portals/feedbacks-admin/midsem/index.js
+++ routes/admin/portals/feedbacks-admin/midsem/index.js
@@ -1,89 +1,99 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fq = require('fuzzquire');
-var feedbacksModel = fq('schemas/feedbacks');
-var adminsModel = fq('schemas/admins');
+var fq = require("fuzzquire");
+var feedbacksModel = fq("schemas/feedbacks");
+var adminsModel = fq("schemas/admins");
 
-router.get('/', function (req, res, next) {
-    try {
-        feedbacksModel.find({type:"midsem"}, (err, feedbacks) => {
-            if (err) {
-                return res.terminate(err);
-            }
-            adminsModel.find({
-                superUser: false
-            }, {
-                __v: 1,
-                name: 1
-            }, {
-                sort: {
-                    name: 1
-                }
-            }, (err, result) => {
-                if (err) {
-                    return res.terminate(err);
-                }
-                res.renderState('admin/portals/feedbacks-admin/midsem/index', {
-                    profs: result,
-                    results: {
-                        feedbacks: feedbacks
-                    }
-                });
-            });
-        });
-    } catch (err) {
+router.get("/", function(req, res, next) {
+  try {
+    feedbacksModel.find({ type: "midsem" }, (err, feedbacks) => {
+      if (err) {
         return res.terminate(err);
-    }
+      }
+      adminsModel.find(
+        {
+          superUser: false
+        },
+        {
+          __v: 1,
+          name: 1
+        },
+        {
+          sort: {
+            name: 1
+          }
+        },
+        (err, result) => {
+          if (err) {
+            return res.terminate(err);
+          }
+          res.renderState("admin/portals/feedbacks-admin/midsem/index", {
+            profs: result,
+            results: {
+              feedbacks: feedbacks
+            }
+          });
+        }
+      );
+    });
+  } catch (err) {
+    return res.terminate(err);
+  }
 });
 
-
 function getUTCDate(epoch) {
-    let utcDate = new Date(epoch);
-    let date = new Date();
-    date.setUTCDate(utcDate.getDate());
-    date.setUTCHours(utcDate.getHours());
-    date.setUTCMonth(utcDate.getMonth());
-    date.setUTCMinutes(utcDate.getMinutes());
-    date.setUTCSeconds(utcDate.getSeconds());
-    date.setUTCMilliseconds(utcDate.getMilliseconds());
-    return date.toLocaleDateString('en-US');
+  let utcDate = new Date(epoch);
+  let date = new Date();
+  date.setUTCDate(utcDate.getDate());
+  date.setUTCHours(utcDate.getHours());
+  date.setUTCMonth(utcDate.getMonth());
+  date.setUTCMinutes(utcDate.getMinutes());
+  date.setUTCSeconds(utcDate.getSeconds());
+  date.setUTCMilliseconds(utcDate.getMilliseconds());
+  return date.toLocaleDateString("en-US");
 }
 
-
-router.get('/view/:id', function (req, res, next) {
-    try {
-        adminsModel.findOne({
-            _id: req.sanitize(req.params.id)
-        }, {
-            name: 1,
-            email: 1
-        }, (err, result) => {
-            if (err) {
+router.get("/view/:id", function(req, res, next) {
+  try {
+    adminsModel.findOne(
+      {
+        _id: req.sanitize(req.params.id)
+      },
+      {
+        name: 1,
+        email: 1
+      },
+      (err, result) => {
+        if (err) {
+          return res.terminate(err);
+        }
+        if (result != null && result != undefined) {
+          feedbacksModel.find(
+            {
+              instructor: result.email,
+              type: "midsem"
+            },
+            (err, feedbacks) => {
+              if (err) {
                 return res.terminate(err);
-            }
-            if (result != null && result != undefined) {
-                feedbacksModel.find({
-                    instructor: result.email,
-                    type:"midsem"
-                }, (err, feedbacks) => {
-                    if (err) {
-                        return res.terminate(err);
-                    }
+              }
 
-                    res.renderState('admin/portals/feedbacks-admin/midsem/view', {
-                        results: {
-                            id: result._id,
-                            name: result.name,
-                            email: result.email,
-                            feedback: feedbacks
-                        }
-                    });
-                });
+              res.renderState("admin/portals/feedbacks-admin/midsem/view", {
+                results: {
+                  id: result._id,
+                  name: result.name,
+                  email: result.email,
+                  feedback: feedbacks
+                }
+              });
             }
-        });
-    } catch (err) {
-        return res.terminate(err);
-    }
+          );
+        }
+      }
+    );
+  } catch (err) {
+    return res.terminate(err);
+  }
 });
 
 module.exports = router;
--- routes/admin/portals/feedbacks-prof/index.js
+++ routes/admin/portals/feedbacks-prof/index.js
@@ -1,49 +1,53 @@
 //24x7 feedback
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fq = require('fuzzquire');
-var feedbacksModel = fq('schemas/feedbacks');
-var adminsModel = fq('schemas/admins');
+var fq = require("fuzzquire");
+var feedbacksModel = fq("schemas/feedbacks");
+var adminsModel = fq("schemas/admins");
 
-router.get('/', function (req, res, next) {
-    try {
-        feedbacksModel.find({
-                instructor: req.user.email,
-                type: "24x7"
-            }, {
-                __v: 0
-            }, {
-                sort: {
-                    createdOn: -1
-                }
-            },
-            (err, result) => {
-                if (err) {
-                    return res.terminate(err);
-                }
-                result.forEach((object, index, array) => {
-                    array[index].createdOn = getUTCDate(Number(array[index].createdOn));
-                });
-                res.renderState('admin/portals/feedbacks-prof', {
-                    feedbacks: result
-                });
-            });
-    } catch (err) {
-        return res.terminate(err);
-    }
+router.get("/", function(req, res, next) {
+  try {
+    feedbacksModel.find(
+      {
+        instructor: req.user.email,
+        type: "24x7"
+      },
+      {
+        __v: 0
+      },
+      {
+        sort: {
+          createdOn: -1
+        }
+      },
+      (err, result) => {
+        if (err) {
+          return res.terminate(err);
+        }
+        result.forEach((object, index, array) => {
+          array[index].createdOn = getUTCDate(Number(array[index].createdOn));
+        });
+        res.renderState("admin/portals/feedbacks-prof", {
+          feedbacks: result
+        });
+      }
+    );
+  } catch (err) {
+    return res.terminate(err);
+  }
 });
 
 function getUTCDate(epoch) {
-    let utcDate = new Date(epoch);
-    let date = new Date();
-    date.setUTCDate(utcDate.getDate());
-    date.setUTCHours(utcDate.getHours());
-    date.setUTCMonth(utcDate.getMonth());
-    date.setUTCMinutes(utcDate.getMinutes());
-    date.setUTCSeconds(utcDate.getSeconds());
-    date.setUTCMilliseconds(utcDate.getMilliseconds());
-    return date.toLocaleDateString('en-US');
+  let utcDate = new Date(epoch);
+  let date = new Date();
+  date.setUTCDate(utcDate.getDate());
+  date.setUTCHours(utcDate.getHours());
+  date.setUTCMonth(utcDate.getMonth());
+  date.setUTCMinutes(utcDate.getMinutes());
+  date.setUTCSeconds(utcDate.getSeconds());
+  date.setUTCMilliseconds(utcDate.getMilliseconds());
+  return date.toLocaleDateString("en-US");
 }
 
 module.exports = router;
