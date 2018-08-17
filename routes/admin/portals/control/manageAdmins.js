f2cc976ef7e24095d9e95a803e0cdbd4586cb8d6
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-08-17 19:12:28 +0530

    control: Add delete admin functionality in manage-admins

--- routes/admin/portals/control/manageAdmins.js
+++ routes/admin/portals/control/manageAdmins.js
@@ -3,9 +3,31 @@ let router = express.Router();
 let fq = require("fuzzquire");
 
 let adminsModel = fq("schemas/admins");
+let portalsModel = fq("schemas/portals");
 
 router.get("/", function(req, res, next) {
-  res.send("Manage admins portal");
+  adminsModel.find(
+    { email: { $ne: req.user.email } },
+    {},
+    { sort: { email: 1 } },
+    function(err, admins) {
+      if (err) return res.terminate("Can not find admins");
+      portalsModel.find({ admin: true }, function(err, portals) {
+        if (err) return res.terminate("Can not find portals");
+        return res.renderState("admin/portals/control/manageAdmins", {
+          admins,
+          portals
+        });
+      });
+    }
+  );
+});
+
+router.post("/delete", function(req, res, next) {
+  adminsModel.remove({ email: req.sanitize(req.body.admin) }, function(err) {
+    if (err) return res.terminate("Could not delete admin");
+    return res.redirect("/admin/control/manage-admins");
+  });
 });
 
 module.exports = router;
