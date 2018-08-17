026b50f091d91a84f954c769cee2f225cd54dec0
Nishant <nishant23799@gmail.com>
2018-08-17 19:43:15 +0530

    manage-users: develop the UI of the add users portal.

--- routes/admin/portals/manage-users/index.js
+++ routes/admin/portals/manage-users/index.js
@@ -2,15 +2,32 @@ const express = require("express");
 const router = express.Router();
 const fq = require("fuzzquire");
 const adminsModel = fq("schemas/admins");
+const portalsModel = fq("schemas/portals");
 
 router.get("/", (req, res, next)=>{
-	res.renderstate("admin/portals/manage-users");
-});
-
-router.get("/adduser",(req, res, next)=>{
-	res.renderstate("admin/portals/manage-users/add");
+	res.renderState("./admin/portals/manage-users");
 });
 
 router.get("/updateuser",(req, res, next)=>{
-	res.renderstate("admin/portals/manage-users/update");
+	res.renderState("admin/portals/manage-users/update");
 })
+
+router.get("/adduser", function(req, res, next) {
+  portalsModel
+    .find({admin:true})
+    .exec(function(err, access) {
+      if (err) {
+        res.terminate(err);
+      }
+      console.log(access);
+      res.renderState("admin/portals/manage-users/add", {
+        access: access
+      });
+    });
+});
+
+router.post("/adduser/add", (req, res, next)=>{
+	
+})
+
+module.exports = router;
