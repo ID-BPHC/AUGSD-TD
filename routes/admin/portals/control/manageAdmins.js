1dc042db642055a6249bdce6e8c7e5b7b57b4163
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-08-18 01:21:57 +0530

    control: Add backend for adding new admin in manage-admins

--- routes/admin/portals/control/manageAdmins.js
+++ routes/admin/portals/control/manageAdmins.js
@@ -23,6 +23,48 @@ router.get("/", function(req, res, next) {
   );
 });
 
+router.post("/add", function(req, res, next) {
+  console.log(req.body);
+  adminsModel.find({ email: req.sanitize(req.body.email) }, function(
+    err,
+    admins
+  ) {
+    if (err) return res.terminate("Could not find admins");
+    if (admins.length == 0) {
+      adminsModel.create(
+        {
+          name: req.sanitize(req.body.name),
+          email: req.sanitize(req.body.email),
+          portals: req.body.portals,
+          home: "/",
+          department:
+            req.sanitize(req.body.department).indexOf("|") >= 0
+              ? req.sanitize(req.body.department).split("|")[1]
+              : "",
+          departmentCode:
+            req.sanitize(req.body.department).indexOf("|") >= 0
+              ? req.sanitize(req.body.department).split("|")[0]
+              : "",
+          maxProjects: parseInt(req.sanitize(req.body.maxProjects)),
+          superUser: false
+        },
+        function(err) {
+          if (err) return res.terminate("Could not save admin");
+          return res.redirect("/admin/control/manage-admins");
+        }
+      );
+    } else {
+      return res.renderState("custom_errors", {
+        redirect: "/admin/control/manage-admins",
+        timeout: 2,
+        supertitle: "Already Exists",
+        message: "This email already exists in the database",
+        details: " "
+      });
+    }
+  });
+});
+
 router.post("/delete", function(req, res, next) {
   adminsModel.remove({ email: req.sanitize(req.body.admin) }, function(err) {
     if (err) return res.terminate("Could not delete admin");
