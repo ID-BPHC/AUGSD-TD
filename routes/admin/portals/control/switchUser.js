84165a393fa04ca8c850ec12f94e1d40c0a861ca
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-08-17 18:46:43 +0530

    control: Init manage-admins portal

--- routes/admin/portals/control/switchUser.js
+++ routes/admin/portals/control/switchUser.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res, next) {
       return res.terminate("Could not find admins");
     }
 
-    return res.renderState("admin/portals/control/switch-user", {
+    return res.renderState("admin/portals/control/switchUser", {
       users: users
     });
   });
