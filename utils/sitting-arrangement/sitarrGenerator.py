5c3f1b25cd389ba2cb7829f93ce67fc7df4e387c
Nishant <nishant23799@gmail.com>
2018-09-06 18:23:10 +0530

    sitting-arrangement: allot the room to each student for a particular course exam.

--- utils/sitting-arrangement/sitarrGenerator.py
+++ utils/sitting-arrangement/sitarrGenerator.py
@@ -23,17 +23,16 @@ dates = []
 roomfields, studentfields, examfields = [], [], []
 rooms, students, exams = [], [], [] 
 pExam = []
+examstudents = []
 
 #Room list 
 with open(room, 'r') as room:
 	roomreader = csv.reader(room)
 
 	roomfields = next(roomreader)
-	roomfields.insert(2,'booked')
-	roomfields.insert(3, 'vacancy')
+	roomfields.append('vacancy')
 
 	for row in roomreader:
-		row.append(False)
 		row[1] = int(row[1])
 		row.append(row[1])
 		rooms.append(row)
@@ -43,8 +42,10 @@ with open(student, 'r') as stu:
 	stureader = csv.reader(stu)
 	
 	studentfields = next(stureader)
+	studentfields.append(room)
 	
 	for row in stureader:
+		row.append("")
 		students.append(row)
 
 #course exam dates and time list
@@ -66,6 +67,52 @@ for exam in exams:
 		dates.append(exam[2])
 
 
+#COUNTS THE NUMBER OF STUDENTS REGISTERED IN THE COURSE
+def stucounter(exam):		
+	count = sum(1 for student in students if student[2] == exam[0])		
+	return count
+
+
+#ROOM SET 
+xs, s, m, l, xl = [], [], [], [], []
+for room in rooms:
+	if room[0].startswith("G"):
+		l.append(room)
+	if room[0].startswith("F") or room[0].startswith("G"):
+		xl.append(room)
+	if room[0].startswith("F1") :
+		m.append(room)	
+	if room[1]<30:
+		xs.append(room)
+	elif room[1] >30 and room[1]<50:
+		s.append(room)
+
+
+def roomallot(exam):
+	if exam[4]<30:
+		return [xs,s,m,l,xl] 
+	elif exam[4]>30 and exam[4]<100:
+		return [s,xs,m,l,xl]
+	elif exam[4]>100 and exam[4]<450:
+		return [m,s,xs,l,xl]
+	elif exam[4]>450 and exam[4]<600:
+		return [l,m,s,xs,xl]
+	else: 
+		return [xl,l,m,s,xs]
+
+#ROOM ALLOTMENT
+def fillstudent(exam,room,examstudents):
+	for student in examstudents:
+		if exam[4] == 0 or room[2] == 0:
+			break
+		if student[2] == exam[0] and student[3] == "":
+			student[3] = room[0]
+			room[2] -= 1
+			exam[4] -= 1	
+				
+
+
+#ALGORITHM
 for date in dates:
 	for session in ["AN", "FN"]: 
 		pExam.clear()
@@ -75,50 +122,38 @@ for date in dates:
 		
 		for exam in pExam: 
 			exam.append(stucounter(exam))
-		
+
 		pExam.sort(key = lambda x:x[4])
 		pExam.reverse()
 
+		for exam in pExam:
+			examstudents.clear()
+			
+			for student in students:
+				if student[2] == exam[0]:
+					examstudents.append(student)
+			
+			examstudents.sort()
+			allot = roomallot(exam)
+			for roomset in allot:
+					
+				if exam[4] == 0:
+					break
+				
+				for room in roomset:
+					if exam[4]==0:
+						break
+					if room[2] == 0:
+						continue
+					if exam[4]<=room[2]:
+						fillstudent(exam, room, examstudents)
+						break
+					else:
+						fillstudent(exam, room, examstudents)
 
-		
 
 
-#COUNTS THE NUMBER OF STUDENTS REGISTERED IN THE COURSE
-def stucounter(exam):		
-	count = sum(1 for student in students if student[2] == exam[0])	
-	print("the no. of the students reg in", exam[0] , "are : ", count)	
-	return count
 
-
-#ROOM ALLOTMENT
-#Rooms set
-xs, s, m, l, xl = [], [], [], [], []
-for room in rooms:
-	if(room[0].startswith("G"))
-		l.append(room)
-	if(room[0].startswith("F") and room[0].startswith("G"))
-		xl.append(room)
-	if(room[0].startswith("F1"))
-		m.append(room)	
-	if room[1]<30
-		xs.append(room)
-	elif room[1] >30 and room <50
-		m.append(room)
-
-	for roomset in [xs,s,m,l,xl]
-		roomset.sort(key)
-		
-def roomallot(exam):
-	if exam[4]<30:
-		return xs 
-	elif exam[4]>30 and exam[4]<100:
-		return s
-	elif exam[4]>100 and exam[4]<450:
-		return m
-	elif exam[4]>450 and exam[4]<600:
-		return l
-	else: 
-		return xl
 
 
 
