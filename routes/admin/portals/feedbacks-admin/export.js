a02c613d8d2185f129e080db5403b66bac73c208
Nishant <nishant23799@gmail.com>
2018-12-21 01:02:39 +0530

    feedbacks: download the csv dump.

--- routes/admin/portals/feedbacks-admin/export.js
+++ routes/admin/portals/feedbacks-admin/export.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fq = require("fuzzquire");
 const fs = require("fs");
 const Json2csvParser = require("json2csv").Parser;
+const path = require("path");
 
 const feedbacksModel = fq("schemas/feedbacks");
 const adminsModel = fq("schemas/admins");
@@ -37,7 +38,7 @@ router.get("/", (req, res, next) => {
 
             feedbacks.forEach((object, index, array) => {
               profs.forEach((obj, ind, arr) => {
-                if (arr[ind].email === array[index].instructor) {
+                if (arr[ind].email === array[index].email) {
                   array[index].InstructorName = arr[ind].name;
                 }
               });
@@ -78,7 +79,28 @@ router.get("/", (req, res, next) => {
             const opts = { fields };
             const parser = new Json2csvParser(opts);
             const csv = parser.parse(feedbacks);
-            console.log(csv);
+            const fileName = feedtype + "Dump__" + Date.now() + ".csv";
+            const relPath = path.join("./public/AUGSD/", fileName);
+            const absPath = path.join(
+              __dirname,
+              "./../../../../public/AUGSD/",
+              fileName
+            );
+            fs.writeFile(relPath, csv, err => {
+              if (err) {
+                console.log(err);
+              }
+              res.download(absPath, err => {
+                if (err) {
+                  console.log(err);
+                }
+                fs.unlink(relPath, err => {
+                  if (err) {
+                    console.log(err);
+                  }
+                });
+              });
+            });
           }
         );
       }
