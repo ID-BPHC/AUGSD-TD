7fff1a394e321c5a6879a98f51abe3fff4e186d2
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-12-20 21:12:27 +0530

    proj-allotment: Switch to dynamic project-allotment guidelines.

--- routes/admin/portals/control/index.js
+++ routes/admin/portals/control/index.js
@@ -8,12 +8,14 @@ let manageAdmins = require("./manageAdmins.js");
 let portalToggle = require("./portals.js");
 let switchUser = require("./switchUser.js");
 let ttExceptions = require("./ttExceptions");
+let project_guidelines = require("./project-allotment-guidelines");
 
 router.use("/holidays", holiday);
 router.use("/manage-admins", manageAdmins);
 router.use("/portal-toggle", portalToggle);
 router.use("/switch-user", switchUser);
 router.use("/tt-exceptions", ttExceptions);
+router.use("/project-allotment-guidelines", project_guidelines);
 
 router.get("/", function(req, res, next) {
   res.renderState("admin/portals/control");
