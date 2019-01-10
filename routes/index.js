45e77234f6e3e30ff6e587c3a89831c2c902ed67
Nishant <nishant23799@gmail.com>
2019-01-10 23:10:49 +0530

    fd-thesis: transfer the student portal to the main page.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var fq = require("fuzzquire");
 var users = fq("users");
 var config = fq("config");
+var path = require("path");
 
 router.get("/", function(req, res, next) {
   res.render("index");
@@ -16,6 +17,24 @@ router.get("/team", function(req, res, next) {
 });
 router.get("/type", function(req, res, next) {
   res.render("type");
+});
+
+router.get("/fd-thesis", (req, res, next) => {
+  res.render("fd-thesis");
+});
+
+var forms = [
+  "Off-Campus-Form-A",
+  "Off-Campus-Form-B",
+  "Pre-allotment-Guidelines"
+];
+
+forms.forEach(form => {
+  router.get("/" + form, (req, res, next) => {
+    res.sendFile(
+      path.join(__dirname + "./../public/AUGSD/fd-thesis/" + form + ".pdf")
+    );
+  });
 });
 
 module.exports = router;
