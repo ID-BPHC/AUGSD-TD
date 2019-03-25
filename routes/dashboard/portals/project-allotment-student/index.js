3fca0d8a43e6a1febc4fd6da1f6190274abfa620
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-03-25 10:21:22 +0530

    project: Update project titles in student portal

--- routes/dashboard/portals/project-allotment-student/index.js
+++ routes/dashboard/portals/project-allotment-student/index.js
@@ -429,9 +429,9 @@ router.post(
       visible: false
     };
     const typeproj = req.sanitize(req.body.projectType);
-    if (typeproj == "Lab Oriented Project (LOP)") {
+    if (typeproj == "Lab Project (LP)") {
       data.type = "lop";
-    } else if (typeproj == "Design Oriented Project (DOP)") {
+    } else if (typeproj == "Design Project (DP)") {
       data.type = "dop";
     } else {
       data.type = "sop";
