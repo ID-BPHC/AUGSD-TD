065f55345461d0a80667566e9e7c85bb2d05c7b5
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-04-03 18:30:09 +0530

    Fix lint for app.js

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ app.use(
   )
 );
 
-morgan.token("user", function (req) {
+morgan.token("user", function(req) {
   if (typeof req.user !== "undefined") return req.user.email;
   else return "";
 });
 
 app.use(
   morgan(":date[clf] :user :remote-addr :user-agent - :method :status :url", {
-    skip: function (req, res) {
+    skip: function(req, res) {
       return (
         req.url.indexOf("/scripts") >= 0 ||
         req.url.indexOf("/stylesheets") >= 0 ||
@@ -58,8 +58,8 @@ var dashboard = require("./routes/dashboard");
 var index = require("./routes");
 
 // A termination function on any kind of error that occours after login
-app.use(function (req, res, next) {
-  res.terminate = function (err) {
+app.use(function(req, res, next) {
+  res.terminate = function(err) {
     bugsModel.create(
       {
         category: "Site",
@@ -67,7 +67,7 @@ app.use(function (req, res, next) {
         student: req.user.email,
         useragent: req.sanitize(req.headers["user-agent"])
       },
-      function (err1, bug) {
+      function(err1, bug) {
         if (err1) {
           console.log(err1);
           res.end();
@@ -91,14 +91,14 @@ app.use("/dashboard", dashboard);
 app.use("/", index);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
