c18d14737edf5166723f38f44abfd7305b58b389
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-05-25 20:57:17 +0530

    Add api for CG Transcripts

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+var v1 = require("./v1/index");
+
+router.use("/v1", v1); // APIs must always be versioned.
+
+module.exports = router;
