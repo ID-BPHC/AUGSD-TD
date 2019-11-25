3a1027909e4ce66fa307b4254b0b0a1f0f885fa7
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-11-25 15:37:20 +0530

    td: re-design team page in bootstrap

--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,25 @@ let users = {
   TD: {
     "Prof. PK Sahoo": {
       name: "Prof. PK Sahoo",
-      title: "Faculty in-charge, Timetable Division",
+      title: "Faculty in-charge",
       avatar: "/images/team/2.jpg",
       social: {}
     },
     "Prof. K Venkata Ratnam": {
       name: "Dr. K Venkata Ratnam",
-      title: "Timetable Nucleus Member",
+      title: "Nucleus Member",
       avatar: "/images/team/17.jpg",
       social: {}
     },
     "Prof. Sashideep Gutti": {
       name: "Dr. Sashideep Gutti",
-      title: "Timetable Nucleus Member",
+      title: "Nucleus Member",
       avatar: "/images/team/20.jpg",
       social: {}
     },
     "Prof. Runa Kumari ": {
       name: "Dr. Runa Kumari",
-      title: "Timetable Nucleus Member",
+      title: "Nucleus Member",
       avatar: "/images/team/21.png",
       social: {}
     },
@@ -36,12 +36,6 @@ let users = {
       avatar: "/images/team/14.jpg",
       social: {}
     },
-    "Nischay Ram Mamidi": {
-      name: "Nischay Ram Mamidi",
-      title: "System Administrator & Software Developer",
-      avatar: "/images/team/15.jpg",
-      social: {}
-    },
     "Sohail Rajdev": {
       name: "Sohail Rajdev",
       title: "System Administrator & Software Developer",
@@ -50,14 +44,8 @@ let users = {
     },
     "Kailash Bhalaki": {
       name: "Kailash Bhalaki",
-      title: "Software Developer",
+      title: "System Administrator & Software Developer",
       avatar: "/images/team/18.jpg",
-      social: {}
-    },
-    "Nishant Aggarwal": {
-      name: "Nishant Aggarwal",
-      title: "Software Developer",
-      avatar: "/images/team/19.jpg",
       social: {}
     }
   },
