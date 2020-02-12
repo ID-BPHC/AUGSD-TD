1066085acc9b3ee382fe3a6885c2951504780efa
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2020-02-12 23:24:04 +0530

    ÃuseNewParser

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(express.static(path.join(__dirname, "public", config.siteMode)));
 
 mongoose.connect(
   config.mongooseConnection,
-  {}
+  { useNewUrlParser: true }
 );
 
 var admin = require("./routes/admin");
