55340c439ac5d5112fdae793107cc797e992678c
vineeth kumar <f20180233@hyderabad.bits-pilani.ac.in>
2020-09-29 20:49:42 +0530

    feedbacks: control feedback emails sent to professors through a config (#122)

--- routes/dashboard/portals/feedbacks-24x7/index.js
+++ routes/dashboard/portals/feedbacks-24x7/index.js
@@ -6,7 +6,9 @@ let router = express.Router();
 let feedbacksModel = fq("schemas/feedbacks");
 let feedbacks = require("./../feedbacks");
 
-["/", "/step-1"].forEach(step => {
+let config = require("../../../../config");
+
+["/", "/step-1"].forEach((step) => {
   router.get(step, feedbacks);
 });
 
@@ -21,7 +23,7 @@ let errorHandler = function(req, res, message) {
     supertitle: ".",
     callback: "/",
     message: "Validation Error",
-    details: message
+    details: message,
   });
 };
 
@@ -54,7 +56,7 @@ router.post("/step-4", function(req, res, next) {
         instructor: instructoremail, // Instructor's email
         type: "24x7", // 24x7 or midsem
         responses: feedback,
-        createdOn: Date.now()
+        createdOn: Date.now(),
       };
       feedbacksModel.create(dataStore, function(err, response) {
         if (err) {
@@ -63,29 +65,31 @@ router.post("/step-4", function(req, res, next) {
             timeout: 5,
             supertitle: "Couldn't submit feedback",
             message: "Failure",
-            details: err
+            details: err,
           });
         }
-        mailer.send({
-          email: instructoremail,
-          subject: "Feedback 24x7",
-          body:
-            "Dear " +
-            instructorname +
-            "<p>AUGSD has received the following qualitative feedback from your students for your course " +
-            courseID +
-            " and section " +
-            courseSection +
-            " through 24 X 7 online portal. You may reflect upon the same and do the needful to enhance the overall environment of teaching and learning in your course. Kindly understand that the feedback is the perception of your student and sometimes may not be well written as they are students. You are requested to ignore those feedbacks which you think don't have any relevance. At the same time, AUGSD would still want to share all the feedback we receive through various means so that you can better understand your students.</p><p><b>" +
-            feedback +
-            "</b></p>"
-        });
+        if (config.sendFeedbackMailToProf) {
+          mailer.send({
+            email: instructoremail,
+            subject: "Feedback 24x7",
+            body:
+              "Dear " +
+              instructorname +
+              "<p>AUGSD has received the following qualitative feedback from your students for your course " +
+              courseID +
+              " and section " +
+              courseSection +
+              " through 24 X 7 online portal. You may reflect upon the same and do the needful to enhance the overall environment of teaching and learning in your course. Kindly understand that the feedback is the perception of your student and sometimes may not be well written as they are students. You are requested to ignore those feedbacks which you think don't have any relevance. At the same time, AUGSD would still want to share all the feedback we receive through various means so that you can better understand your students.</p><p><b>" +
+              feedback +
+              "</b></p>",
+          });
+        }
         res.renderState("custom_errors", {
           redirect: "/dashboard",
           timeout: 2,
           supertitle: "Submitted Feedback.",
           message: "Success",
-          details: "Your feedback was recorded. Thank you :). Redirecting"
+          details: "Your feedback was recorded. Thank you :). Redirecting",
         });
       });
     }
