3efbf583c7e18cd22668ccc846d152528fd0154f
Yash Saravgi <yashsaravgi911@gmail.com>
2023-02-13 12:00:54 +0530

    Add new previous booking Page

--- routes/admin/portals/previous-room-booking/index.js
+++ routes/admin/portals/previous-room-booking/index.js
@@ -7,14 +7,14 @@ let mapByDays = require("../common").mapByDays;
 
 router.get("/", function(req, res, next) {
   bookingsModel.find(
-    { end: { $gt: moment() }, approval: "A", blockAll: false },
+    { end: { $gt: moment().subtract(1, 'months'), $lt: moment() }, approval: "A", blockAll: false },
     null,
     { sort: { start: 1 } },
     function(err, bookings) {
       if (err) {
         return res.terminate(err);
       }
-      return res.renderState("admin/portals/room-booking-all", {
+      return res.renderState("admin/portals/previous-room-booking", {
         bookings: mapByDays(bookings)
       });
     }
